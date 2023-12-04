66b683bd4e73d2324259352ae1f64f35b6a3226a
ChiaraPontello <chiarapontello1@gmail.com>
2023-12-04 15:42:30 +0100

    function construct + var_dump movies

--- Model/Movie.php
+++ Model/Movie.php
@@ -7,7 +7,25 @@ class Movie {
     private $vote_average;
     private $poster_path;
     private $original_language;
+
+    function __construct($id, $title, $overview, $vote, $language, $image) 
+    {
+        $this ->id = $id;
+        $this -> title = $title;
+        $this ->overview = $overview;   
+        $this ->vote_average = $vote;
+        $this ->poster_path = $image;
+        $this ->original_language= $language;
+    }
 }
-$Babylon = new Movie();
-var_dump($Babylon);
+
+$movieString = file_get_contents(__DIR__ .'/movie_db.json');
+$movieList = json_decode($movieString, true);
+
+$movies = [];
+
+foreach( $movieList as $item ){
+    $movies[] = new Movie($item ['id'], $item ['title'],$item ['overview'],$item ['vote_average'], $item ['original_language'], $item ['poster_path']);
+}
+var_dump($movies);
 ?>
